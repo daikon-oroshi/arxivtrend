03126b8f010923f7c90ef06108a6f25445835e05
oroshi <oroshinekko@gmail.com>
2025-01-22 17:56:35 +0900

    argparse -> click

--- arxivtrend/main.py
+++ arxivtrend/main.py
@@ -1,54 +1,112 @@
+import click
+from datetime import date
 from arxivtrend.apps import SearchUsecase
 from arxivtrend.domain.entities import (
     ArxivQueryEntity
 )
-from arxivtrend.args import parser, Commands
 
 
-def parse_search_query(args) -> ArxivQueryEntity:
-    return ArxivQueryEntity(
-        search_q=args.q,
-        submitted_begin=args.submitted_begin,
-        submitted_end=args.submitted_end,
-        category=args.category
+@click.group()
+def cli():
+    pass
+
+
+dec_search_q_arg = click.argument(
+        'search_query',
+        type=click.STRING,
     )
 
 
-def delete(args):
-    query = parse_search_query(args)
+dec_begin_arg = click.option(
+        '-b',
+        'begin',
+        type=click.DateTime(),
+        default=date(1991, 1, 1),
+        help='ex. 2020-01-01.'
+    )
+
+
+dec_end_arg = click.option(
+        '-e',
+        'end',
+        type=click.DateTime(),
+        default=date.today(),
+        help='ex. 2045-12-31.'
+    )
+
+
+dec_category_arg = click.option(
+        "-c", "--category",
+        type=click.STRING,
+        required=False,
+        default=""
+    )
+
+
+dec_force_reacquire = click.option(
+        "-f",
+        "--force_reacquire",
+        is_flag=True,
+        help='force reacquisition even if arxiv data was cached.'
+    )
+
+
+@cli.command()
+@dec_search_q_arg
+@dec_begin_arg
+@dec_end_arg
+@dec_category_arg
+@dec_force_reacquire
+def search(
+    search_query: str,
+    begin: date | None,
+    end: date | None,
+    category: str,
+    force_reacquire: bool = False
+):
+    query = ArxivQueryEntity(
+        search_q=search_query,
+        submitted_begin=begin,
+        submitted_end=end,
+        category=category
+    )
+    usecase = SearchUsecase()
+    usecase.search_and_cache_arxiv(
+        query,
+        force_reacquire
+    )
+
+
+@cli.command()
+@dec_search_q_arg
+@dec_begin_arg
+@dec_end_arg
+@dec_category_arg
+def delete(
+    q: str,
+    begin: date | None,
+    end: date | None,
+    category: str,
+):
+    query = ArxivQueryEntity(
+        search_q=q,
+        submitted_begin=begin,
+        submitted_end=end,
+        category=category
+    )
     usecase = SearchUsecase()
     usecase.delete_cache(query)
 
 
+@cli.command()
 def delete_all():
     usecase = SearchUsecase()
     usecase.delete_cache_all()
 
 
-def search(args):
-    query = parse_search_query(args)
-    usecase = SearchUsecase()
-    usecase.search_and_cache_arxiv(
-        query, args.force_reacquire
-    )
-
-
-def create_report(args):
+def create_report():
     pass
 
 
-def main():
-    args = parser.parse_args()
-    if args.command == Commands.DELETE.value:
-        delete(args)
-    elif args.command == Commands.DELETE_ALL.value:
-        delete_all()
-    elif args.command == Commands.REPORT_ONLY.value:
-        create_report(args)
-    else:
-        search(args)
-        create_report(args)
-
-
 if __name__ == "__main__":
-    main()
+    cli()
